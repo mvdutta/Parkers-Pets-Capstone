8351e7945f0e909da154e8308a6bfc4d851a3ba3
Maia Dutta <mermaia33@gmail.com>
2022-10-27 18:28:32 -0500

    useEffect turned off on profiles

--- src/components/ClientProfile/ClientProfile.js
+++ src/components/ClientProfile/ClientProfile.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavBar } from '../NavBar/NavBar'
 
 export const ClientProfile = () => {
+  const [currentUser, setCurrentUser] = useState()
+
+  // useEffect(()=> {
+  //     const localParkerUser = localStorage.getItem("parker_user")
+  //     setCurrentUser(JSON.parse(localParkerUser))
+  // }, [])
   return (
     <div>
         <NavBar/>
         <h1>Client Profile</h1>
+        {/* <p>Welcome {currentUser.fullName}</p> */}
     </div>
   )
 }
