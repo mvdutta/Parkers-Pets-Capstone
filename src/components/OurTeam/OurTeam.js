730620d54f3866cb287c7b8728ab29bb18a534d2
Maia Dutta <mermaia33@gmail.com>
2022-11-03 21:31:32 -0500

    team cards displaying

--- src/components/OurTeam/OurTeam.js
+++ src/components/OurTeam/OurTeam.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavBar } from "../NavBar/NavBar";
 import styles from "./OurTeam.module.css";
 import { TeamCard } from "./TeamCards";
 
 export const OurTeam = () => {
+
+const [team, setTeam] = useState([])
+
+useEffect(() => {
+    fetch(`http://localhost:8088/employees?_expand=user&_expand=petType`)
+    .then(res => res.json())
+    .then((data) => {
+        setTeam(data)
+    })
+}, [])
+
+const teamCards = team.map(teamMember => (
+    <TeamCard
+            key={`teamMember--${teamMember.id}`}
+            image = {teamMember.profileImage}
+            name = {teamMember.user.fullName}
+            petType = {teamMember.petType.type}
+            meds = "*Able to give meds/vet tech"
+            logo = "/cat-icon.png"
+            />
+))
   return (
     <>
       <NavBar />
       <h1>Meet Our Pet Sitters!</h1>
-      <div class="container">
+      <div className="container">
           <div className={styles.cardHolder}>
-            <TeamCard
-            image = "https://cdn.pixabay.com/photo/2017/08/02/10/01/people-2570587__340.jpg"
-            name = "Selena Kyle"
-            petType = "Cats"
-            meds = "*Able to give meds/vet tech"
-            logo = "/cat-icon.png"
-            />
+            {teamCards}
          </div>
           
       </div>
