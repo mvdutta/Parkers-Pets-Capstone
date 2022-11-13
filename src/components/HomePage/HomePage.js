9c7a77d8e0b577ddb93d329add8b01caee8a8859
Maia Dutta <mermaia33@gmail.com>
2022-11-12 21:20:33 -0600

    added icons in services area

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -90,15 +90,24 @@ export const HomePage = () => {
         </div>
     </div>
     <div className='divider-wrapper'>
-        <div className="custom-shape-divider-top-1666820762">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-        </svg>
+        <div className="serviceSection">
         <h1 className='servicesHeader'>Our Services</h1>  
-        </div>
+        <h2>We offer daily pet sitting in your home with simple and straightfoward pricing.</h2>
         <div className='servicesContent'>
-      
-    </div>
+            <div className='serviceIconArea'>
+                <img src="/poodle-icon.png" className='serviceIcon' />
+                <p>Dogs</p>
+            </div>
+            <div className='serviceIconArea'>
+                <img src="/cat-icon2.png" className='serviceIcon'/>
+                <p>Cats</p>
+            </div>
+            <div className='serviceIconArea'>
+                <img src="/fish-icon.png" className='serviceIcon'/>
+                <p>Other Pets</p>
+            </div>
+        </div>
+        </div>      
     </div>
 
 
