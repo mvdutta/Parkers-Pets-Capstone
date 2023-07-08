6b7f762c98d542445f77e56461801ae49dfb36e1
Maia Dutta <mermaia33@gmail.com>
2023-07-07 23:05:52 -0500

    fixed issue with making appointments

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-// export const API = "https://parkers.glitch.me"
-export const API = "http://localhost:8088"
+export const API = "https://parkers-petsitting-api.glitch.me"
+// export const API = "http://localhost:8088"
